6beb085619ef995b96fee67adfce190d4602a2ea
_xBott <roma.plakhotniuk07@gmail.com>
2025-09-29 00:55:50 +0200

    changed version, added env vars and so

--- breeze-paper/build.gradle.kts
+++ breeze-paper/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("xyz.jpenilla.run-paper") version "2.3.1"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
+    id("com.gradleup.shadow") version "8.3.0"
 }
 
 repositories {
@@ -16,4 +17,24 @@ dependencies {
     annotationProcessor(project(":breeze-processor"))
 
     paperweight.paperDevBundle("1.21.7-R0.1-SNAPSHOT")
+}
+
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.shadowJar {
+    dependencies {
+        include(project(":breeze-core"))
+        include(project(":breeze-api"))
+    }
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("paper-plugin.yml") {
+        expand(props)
+    }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,29 @@
+import io.github.klahap.dotenv.DotEnvBuilder
+
 plugins {
     id("java")
+    id("io.github.klahap.dotenv") version "1.1.3"
+    id("maven-publish")
 }
 
 allprojects {
     group = "me.bottdev"
-    version = "0.1"
+    version = "0.1.4"
 
     repositories {
         mavenCentral()
     }
 }
 
+val envVars = DotEnvBuilder.dotEnv {
+    addFile("$rootDir/.env")
+}
+extra["envVars"] = envVars
+
 subprojects {
     apply(plugin = "java")
+    apply(plugin = "io.github.klahap.dotenv")
+    apply(plugin = "maven-publish")
 
     dependencies {
         testImplementation(platform("org.junit:junit-bom:5.10.0"))
@@ -27,5 +38,31 @@ subprojects {
 
     tasks.test {
         useJUnitPlatform()
+    }
+
+    val envVars = rootProject.extra["envVars"] as Map<String, String>
+
+    publishing {
+        repositories {
+            maven {
+                name = project.name
+                url = uri("https://repo.the-light.online/releases")
+                credentials {
+                    username = envVars["REPO_USERNAME"].toString()
+                    password = envVars["REPO_PASSWORD"].toString()
+                }
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+        }
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "me.bottdev"
+                artifactId = project.name
+                version = "${project.version}"
+                from(components["java"])
+            }
+        }
     }
 }
