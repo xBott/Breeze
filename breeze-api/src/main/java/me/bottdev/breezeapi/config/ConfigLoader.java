5502720996d39bbbb565d197e44bf99a8b0fef5f
_xBott <roma.plakhotniuk07@gmail.com>
2025-09-29 00:58:35 +0200

    added some new methods

--- breeze-api/src/main/java/me/bottdev/breezeapi/config/ConfigLoader.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/config/ConfigLoader.java
@@ -3,17 +3,14 @@ package me.bottdev.breezeapi.config;
 import lombok.RequiredArgsConstructor;
 import me.bottdev.breezeapi.config.validation.ConfigStatus;
 import me.bottdev.breezeapi.config.validation.ConfigValidator;
-import me.bottdev.breezeapi.config.validation.FieldStatus;
 import me.bottdev.breezeapi.log.BreezeLogger;
 import me.bottdev.breezeapi.log.SimpleLogger;
 import me.bottdev.breezeapi.serialization.Mapper;
 import me.bottdev.breezeapi.serialization.ObjectNode;
 
-import javax.swing.text.html.Option;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 @RequiredArgsConstructor
 public class ConfigLoader {
@@ -52,8 +49,42 @@ public class ConfigLoader {
         return builder.toString();
 
     }
+    private void writeFile(File file, String content) {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+
+            writer.write(content);
+
+        } catch (Exception ex) {
+            logger.error("Could not write config file.", ex);
+        }
+
+    }
+
+    public <T extends Configuration> boolean canLoad(File file, Class<T> clazz) {
+        String serialized = readFile(file);
+        if (serialized.isBlank()) {
+            return false;
+        }
+
+        Optional<ObjectNode> objectNodeOptional = serializationStrategy.deserializeTree(serialized);
+        if (objectNodeOptional.isEmpty()) {
+            return false;
+        }
+        ObjectNode objectNode = objectNodeOptional.get();
+
+        ConfigStatus configStatus = configValidator.validateTree(objectNode, clazz);
+        if (configStatus != ConfigStatus.SUCCESS) {
+            return false;
+        }
+
+        return true;
+    }
+
 
     public <T extends Configuration> Optional<T> loadConfig(File file, Class<T> clazz) {
+
+        logger.info("Loading config {} from file {}", clazz.getSimpleName(), file.getName());
 
         String serialized = readFile(file);
         if (serialized.isBlank()) {
@@ -74,7 +105,52 @@ public class ConfigLoader {
             return Optional.empty();
         }
 
+        logger.info("Loaded config {} from file {}", clazz.getSimpleName(), file.getName());
+
         return serializationStrategy.deserialize(clazz, serialized);
+
+    }
+
+    public void saveConfig(File file, Configuration configuration) {
+        String serialized = serializationStrategy.serialize(configuration);
+        createFileIfNotExists(file);
+        writeFile(file, serialized);
+        logger.info("Saved config {} into file {}", configuration.getClass().getSimpleName(), file.getName());
+    }
+
+    public <T extends Configuration> T loadOrCreateConfig(File file, Class<T> clazz, Supplier<T> supplier) {
+        logger.info("Loading or creating config {} from file {}", clazz.getSimpleName(), file.getName());
+        Optional<T> configOptional = loadConfig(file, clazz);
+        if (configOptional.isPresent()) {
+            return configOptional.get();
+        }
+        logger.info("Creating config {} from file {}", clazz.getSimpleName(), file.getName());
+
+        Configuration suppliedConfiguration = supplier.get();
+        saveConfig(file, suppliedConfiguration);
+
+        return supplier.get();
+    }
+
+    public <T extends Configuration> T loadOrDefault(File file, Class<T> clazz, Supplier<T> supplier) {
+        logger.info("Loading or default config {} from file {}", clazz.getSimpleName(), file.getName());
+        Optional<T> configOptional = loadConfig(file, clazz);
+        if (configOptional.isPresent()) {
+            return configOptional.get();
+        }
+        logger.info("Passing default config for {}", clazz.getSimpleName());
+
+        return supplier.get();
+    }
+
+    public <T extends Configuration> T loadConfigSafely(File file, Class<T> clazz, Supplier<T> supplier) {
+        logger.info("Loading config safely {} from file {}", clazz.getSimpleName(), file.getName());
+
+        if (!file.exists()) {
+            saveConfig(file, supplier.get());
+            return supplier.get();
+        }
+        return loadOrDefault(file, clazz, supplier);
 
     }
 
