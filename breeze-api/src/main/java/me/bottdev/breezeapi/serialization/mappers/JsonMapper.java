7ef264a798e6472c3bedde469c545bc15d6612c5
_xBott <roma.plakhotniuk07@gmail.com>
2025-09-29 00:57:46 +0200

    changed properties of object mapper

--- breeze-api/src/main/java/me/bottdev/breezeapi/serialization/mappers/JsonMapper.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/serialization/mappers/JsonMapper.java
@@ -1,5 +1,7 @@
 package me.bottdev.breezeapi.serialization.mappers;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import lombok.Getter;
@@ -21,6 +23,7 @@ public class JsonMapper implements JacksonMapper {
         registry.getTypes().forEach(type -> {
             SimpleModule module = type.getModule();
             objectMapper.registerModule(module);
+            objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
         });
         return objectMapper;
     }
