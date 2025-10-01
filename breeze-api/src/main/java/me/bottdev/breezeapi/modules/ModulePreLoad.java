35a116f2f9b3250dfdf9c032ead684b496286708
_xBott <roma.plakhotniuk07@gmail.com>
2025-10-01 22:48:11 +0200

    lazy module creation

--- breeze-api/src/main/java/me/bottdev/breezeapi/modules/ModulePreLoad.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/modules/ModulePreLoad.java
@@ -5,13 +5,19 @@ import lombok.RequiredArgsConstructor;
 import me.bottdev.breezeapi.di.index.ComponentIndex;
 import me.bottdev.breezeapi.di.index.SupplierIndex;
 
+import java.util.Optional;
+import java.util.function.Supplier;
+
 @RequiredArgsConstructor
 @Getter
 public class ModulePreLoad {
 
     private final ClassLoader classLoader;
-    private final Module module;
     private final SupplierIndex supplierIndex;
     private final ComponentIndex componentIndex;
+
+    private final Class<? extends Module> moduleClass;
+    private final Supplier<Optional<Module>> moduleSupplier;
+
 
 }
