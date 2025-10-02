6d9afdfe86a166be1ab99effd1d2c29662ac3bf4
_xBott <roma.plakhotniuk07@gmail.com>
2025-10-02 13:04:22 +0200

    autoload test

--- breeze-admin/src/main/java/me/bottdev/breezeadmin/config/AdminsConfiguration.java
+++ breeze-admin/src/main/java/me/bottdev/breezeadmin/config/AdminsConfiguration.java
@@ -3,10 +3,12 @@ package me.bottdev.breezeadmin.config;
 import lombok.Getter;
 import lombok.Setter;
 import me.bottdev.breezeapi.config.Configuration;
+import me.bottdev.breezeapi.config.autoload.annotations.AutoLoad;
 import me.bottdev.breezeapi.config.validation.annotations.Range;
 
 @Getter
 @Setter
+@AutoLoad(path = "{module}/admin.json")
 public class AdminsConfiguration implements Configuration {
 
     @Range(min = 1, max = 100)
