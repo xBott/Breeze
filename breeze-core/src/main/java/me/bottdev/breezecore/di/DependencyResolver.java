175f14cabbc466402965202a10c9f7dabe5fab58
_xBott <roma.plakhotniuk07@gmail.com>
2025-10-11 03:37:56 +0200

    new resolving algorithm: Kahn's algorithm

--- breeze-core/src/main/java/me/bottdev/breezecore/di/DependencyResolver.java
+++ breeze-core/src/main/java/me/bottdev/breezecore/di/DependencyResolver.java
@@ -1,5 +1,6 @@
 package me.bottdev.breezecore.di;
 
+import me.bottdev.breezeapi.commons.structures.graph.GraphEdge;
 import me.bottdev.breezeapi.commons.structures.graph.GraphNode;
 import me.bottdev.breezeapi.commons.structures.graph.directed.DirectedGraph;
 import me.bottdev.breezeapi.di.SupplyType;
@@ -10,7 +11,7 @@ import me.bottdev.breezeapi.log.SimpleLogger;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class DependencyResolver {
 
@@ -58,30 +59,53 @@ public class DependencyResolver {
         return graph;
     }
 
+
+
     private static List<ResolvedDependency> applyTopographicalSort(DirectedGraph<String> graph) {
 
-        logger.info("Making topographical sort using DFS...");
+        logger.info("Making topographical sort using Kahn's algorithm...");
 
         List<ResolvedDependency> sorted = new ArrayList<>();
 
-        Optional<GraphNode<String>> leastEdgesNodeOptional = graph.getNodeWithLeastEdges();
-        if (leastEdgesNodeOptional.isEmpty()) {
-            throw new IllegalArgumentException("Failed to build topographical sort using DFS.");
+        HashMap<GraphNode<String>, Integer> inDegree = new HashMap<>();
+        for (GraphEdge<String> edge : graph.getEdges()) {
+            GraphNode<String> from = edge.getFrom();
+            GraphNode<String> to = edge.getTo();
+            inDegree.put(to, inDegree.getOrDefault(to, 0));
+            inDegree.put(from, inDegree.getOrDefault(from, 0) + 1);
         }
 
-        GraphNode<String> leastEdgesNode = leastEdgesNodeOptional.get();
+        LinkedBlockingQueue<GraphNode<String>> queue = new LinkedBlockingQueue<>();
+        for (GraphNode<String> node : graph.getNodes()) {
+            int degree = inDegree.get(node);
+            logger.info("- Graph node {} has {} dependencies", node.getValue(), degree);
+            if (degree == 0) {
+                queue.add(node);
+            }
+        }
 
-        graph.dfs(leastEdgesNode, node -> {
-                sorted.add(
-                        new ResolvedDependency(
-                                node.getValue(),
-                                (SupplyType) node.getAttributes().get("supplyType")
-                        )
-                );
-                logger.info("- Added dependency {} to sort list.", node.getValue());
-        });
+        while (!queue.isEmpty()) {
+            GraphNode<String> node = queue.poll();
 
-        logger.info("Successfully built topographical sort using DFS.");
+            sorted.add(new ResolvedDependency(
+                    node.getValue(),
+                    (SupplyType) node.getAttributes().get("supplyType")
+            ));
+            logger.info("- Added dependency {} to sort list.", node.getValue());
+
+            for (GraphNode<String> neighbor : graph.getIncomingNeighbors(node)) {
+                inDegree.put(neighbor, inDegree.get(neighbor) - 1);
+                if (inDegree.get(neighbor) == 0) {
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        if (sorted.size() < graph.getNodes().size()) {
+            throw new IllegalArgumentException("Failed to build topographical sort: graph contains a cycle.");
+        }
+
+        logger.info("Successfully built topographical sort using Kahn's algorithm.");
 
         return sorted;
     }
