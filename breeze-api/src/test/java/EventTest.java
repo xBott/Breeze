191dab672a215cc2baa593057023b7d65f710cd1
_xBott <roma.plakhotniuk07@gmail.com>
2025-11-25 22:41:47 +0100

    better logging

--- breeze-api/src/test/java/EventTest.java
+++ breeze-api/src/test/java/EventTest.java
@@ -3,7 +3,9 @@ import lombok.RequiredArgsConstructor;
 import me.bottdev.breezeapi.events.Cancellable;
 import me.bottdev.breezeapi.events.Event;
 import me.bottdev.breezeapi.events.EventBus;
-import me.bottdev.breezeapi.events.annotations.Listener;
+import me.bottdev.breezeapi.events.Listener;
+import me.bottdev.breezeapi.events.annotations.Listen;
+import me.bottdev.breezeapi.log.SimpleLogger;
 import org.junit.jupiter.api.Test;
 
 public class EventTest {
@@ -28,15 +30,15 @@ public class EventTest {
 
     }
 
-    public static class JoinListener {
+    public static class JoinListener implements Listener {
 
-        @Listener(priority = 2)
+        @Listen(priority = 2)
         public void onJoin(JoinEvent event) {
             if (event.isCancelled()) return;
             System.out.println("Joined: " + event.getName());
         }
 
-        @Listener(priority = 1)
+        @Listen(priority = 1)
         public void onJoinMorePriority(JoinEvent event) {
             System.out.println("NO, NOT JOINED: " + event.getName());
             event.setCancelled(true);
@@ -47,7 +49,7 @@ public class EventTest {
     @Test
     public void testEventBus() {
 
-        EventBus eventBus = new EventBus();
+        EventBus eventBus = new EventBus(new SimpleLogger("EventBus"));
 
         JoinListener listener = new JoinListener();
         eventBus.registerListeners(listener);
