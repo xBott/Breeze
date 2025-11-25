e2758e027200e1a0002276b53720b3b6bdb98ff8
_xBott <roma.plakhotniuk07@gmail.com>
2025-11-25 22:41:13 +0100

    changed fields

--- breeze-api/src/main/java/me/bottdev/breezeapi/BreezeEngine.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/BreezeEngine.java
@@ -1,9 +1,11 @@
 package me.bottdev.breezeapi;
 
+import me.bottdev.breezeapi.autoload.AutoLoaderRegistry;
 import me.bottdev.breezeapi.di.BreezeContext;
 import me.bottdev.breezeapi.events.EventBus;
 import me.bottdev.breezeapi.index.BreezeIndexLoader;
 import me.bottdev.breezeapi.log.BreezeLogger;
+import me.bottdev.breezeapi.log.TreeLogger;
 import me.bottdev.breezeapi.modules.ModuleManager;
 import me.bottdev.breezeapi.serialization.mappers.JsonMapper;
 
@@ -11,15 +13,17 @@ import java.nio.file.Path;
 
 public interface BreezeEngine {
 
+    TreeLogger getLogger();
+
     BreezeIndexLoader getIndexLoader();
 
     BreezeContext getContext();
 
+    AutoLoaderRegistry getAutoLoaderRegistry();
+
     ModuleManager getModuleManager();
 
     EventBus getEventBus();
-
-    BreezeLogger getLogger();
 
     JsonMapper getJsonMapper();
 
