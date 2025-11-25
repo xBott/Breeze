543a308e9cf0d1e016921b435ba0ab6c8bca57b5
_xBott <roma.plakhotniuk07@gmail.com>
2025-11-25 22:37:28 +0100

    preparing for autoload remake

--- breeze-api/src/main/java/me/bottdev/breezeapi/autoload/AutoLoad.java
+++
@@ -1,3 +0,0 @@
-package me.bottdev.breezeapi.autoload;
-
-public @interface AutoLoad {}
---
+++ breeze-api/src/main/java/me/bottdev/breezeapi/autoload/AutoLoader.java
@@ -0,0 +1,7 @@
+package me.bottdev.breezeapi.autoload;
+
+public interface AutoLoader {
+
+    void load(Object object);
+
+}
