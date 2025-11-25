7c3cde7edcfb632fb35fd7c51a861b0ae364fc07
_xBott <roma.plakhotniuk07@gmail.com>
2025-11-25 22:41:35 +0100

    fixed serialization bug

--- breeze-api/src/main/java/me/bottdev/breezeapi/dependency/Dependent.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/dependency/Dependent.java
@@ -1,9 +1,12 @@
 package me.bottdev.breezeapi.dependency;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.List;
 
 public interface Dependent {
 
+    @JsonIgnore
     String getDependentId();
 
     List<String> getDependencies();
