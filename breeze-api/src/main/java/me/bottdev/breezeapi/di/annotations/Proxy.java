a0c3804494b9102d944882b7cc011675569feb6e
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-09 22:43:47 +0100

    fixes

--- breeze-api/src/main/java/me/bottdev/breezeapi/di/annotations/Proxy.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/di/annotations/Proxy.java
@@ -1,7 +1,5 @@
 package me.bottdev.breezeapi.di.annotations;
 
-import me.bottdev.breezeapi.di.SupplyType;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
--- breeze-api/src/main/java/me/bottdev/breezeapi/di/proxy/ProxyHandler.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/di/proxy/ProxyHandler.java
@@ -3,6 +3,7 @@ package me.bottdev.breezeapi.di.proxy;
 import java.lang.reflect.InvocationHandler;
 
 public interface ProxyHandler extends InvocationHandler {
+
     boolean supports(Class<?> iface);
 
 }
