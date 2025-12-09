0a92f89f519cd5505af7c7bf596e1f732477781b
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-09 22:45:27 +0100

    resource test

---
+++ breeze-admin/src/main/java/me/bottdev/breezeadmin/providers/SettingsProvider.java
@@ -0,0 +1,26 @@
+package me.bottdev.breezeadmin.providers;
+
+import me.bottdev.breezeapi.di.annotations.Proxy;
+import me.bottdev.breezeapi.resource.ResourceProvider;
+import me.bottdev.breezeapi.resource.Source;
+import me.bottdev.breezeapi.resource.annotations.Drive;
+import me.bottdev.breezeapi.resource.annotations.FallbackMethod;
+import me.bottdev.breezeapi.resource.annotations.ProvideResource;
+import me.bottdev.breezeapi.resource.types.FileResource;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+@Proxy
+public interface SettingsProvider extends ResourceProvider {
+
+    default FileResource getSettingsFallback() {
+        return new FileResource(Path.of("settings.json"), new byte[0]);
+    }
+
+    @ProvideResource(source = Source.DRIVE)
+    @Drive(path = "{engine}/Admin/settings.json")
+    @FallbackMethod(name = "getSettingsFallback")
+    Optional<FileResource> getSettings();
+
+}
