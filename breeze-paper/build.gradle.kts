9e83b0151dc845141333ef667ed591398932836b
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-09 22:45:59 +0100

    minor changes

--- breeze-paper/build.gradle.kts
+++ breeze-paper/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
+import kotlin.jvm.java
+
 plugins {
     id("xyz.jpenilla.run-paper") version "2.3.1"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
@@ -12,18 +15,26 @@ repositories {
 }
 
 dependencies {
-    implementation(project(":breeze-core"))
-    implementation(project(":breeze-api"))
-    annotationProcessor(project(":breeze-processor"))
 
     paperweight.paperDevBundle("1.21.7-R0.1-SNAPSHOT")
+
+    annotationProcessor(project(":breeze-processor"))
+    implementation(project(":breeze-core"))
+    implementation(project(":breeze-api"))
+
 }
 
 tasks.build {
+    configurations.annotationProcessor.let { an ->
+        an.configure {
+
+        }
+    }
     dependsOn("shadowJar")
 }
 
 tasks.shadowJar {
+    transform(ServiceFileTransformer::class.java)
     dependencies {
         include(project(":breeze-core"))
         include(project(":breeze-api"))
