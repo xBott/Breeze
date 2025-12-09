ffc4747bcb5344f657e0276ece83e5401c7ffbe1
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-09 23:04:53 +0100

    made it simpler

--- breeze-api/src/main/java/me/bottdev/breezeapi/resource/annotations/FallbackHandler.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/resource/annotations/FallbackHandler.java
@@ -10,6 +10,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface FallbackHandler {
-    Class<? extends ResourceFallbackHandler<?>> value();
+    Class<? extends ResourceFallbackHandler> value();
 
 }
