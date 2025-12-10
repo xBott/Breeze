eb5401e19ebc0ee30f32a371adabb2a886ff1d85
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-10 23:26:19 +0100

    better invocation handler creation

--- breeze-api/src/main/java/me/bottdev/breezeapi/di/proxy/ProxyHandlerRegistry.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/di/proxy/ProxyHandlerRegistry.java
@@ -1,10 +1,16 @@
 package me.bottdev.breezeapi.di.proxy;
 
+import me.bottdev.breezeapi.di.proxy.composite.CompositeProxyHandler;
+import me.bottdev.breezeapi.log.BreezeLogger;
+import me.bottdev.breezeapi.log.SimpleLogger;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class ProxyHandlerRegistry {
+
+    private final BreezeLogger logger = new SimpleLogger("ProxyHandlerRegistry");
 
     private final List<Class<? extends ProxyHandler>> HANDLERS = new ArrayList<>();
 
@@ -13,18 +19,51 @@ public class ProxyHandlerRegistry {
         return this;
     }
 
-    public Optional<ProxyHandler> get(Class<?> iface) {
-        for (Class<? extends ProxyHandler> handlerClass : HANDLERS) {
-            try {
-                ProxyHandler handler = handlerClass.getConstructor(Class.class).newInstance(iface);
-                if (handler.supports(iface)) {
-                    return Optional.of(handler);
-                }
-            } catch (Exception e) {
-                return Optional.empty();
-            }
+    private Optional<ProxyHandler> tryCreate(Class<?> clazz, Class<?> iface) {
+
+        try {
+
+            ProxyHandler handler = (ProxyHandler) clazz
+                    .getConstructor(Class.class)
+                    .newInstance(iface);
+
+            return handler.supports(iface) ? Optional.of(handler) : Optional.empty();
+
+        } catch (ReflectiveOperationException ex) {
+            logger.error("Could not create proxy handler for " + iface.getName(), ex);
         }
+
         return Optional.empty();
+    }
+
+
+    public Optional<ProxyHandler> get(Class<?> iface) {
+
+        for (Class<? extends ProxyHandler> handlerClass : HANDLERS) {
+
+            Optional<ProxyHandler> handler = tryCreate(handlerClass, iface);
+            if (handler.isPresent()) {
+                return handler;
+            }
+
+        }
+
+        return Optional.empty();
+    }
+
+    public CompositeProxyHandler getComposite(Class<?> iface) {
+
+        CompositeProxyHandler compositeHandler = new CompositeProxyHandler(iface);
+
+        HANDLERS.stream()
+                .map(clazz -> tryCreate(clazz, iface))
+                .forEach(optional -> optional.ifPresent(
+                        handler -> compositeHandler.add(handler, 0)
+                ));
+
+
+        return compositeHandler;
+
     }
 
 }
