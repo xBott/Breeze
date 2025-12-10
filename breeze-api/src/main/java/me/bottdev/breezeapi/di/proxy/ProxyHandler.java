26b0a8f8616a732a88dbc88a557ca48e4c946f38
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-10 23:26:01 +0100

    changed handler structure and methods

--- breeze-api/src/main/java/me/bottdev/breezeapi/di/proxy/ProxyHandler.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/di/proxy/ProxyHandler.java
@@ -1,9 +1,11 @@
 package me.bottdev.breezeapi.di.proxy;
 
-import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 
-public interface ProxyHandler extends InvocationHandler {
+public interface ProxyHandler {
 
     boolean supports(Class<?> iface);
+
+    Object invoke(Class<?> targetClass, Object proxy, Method method, Object[] args) throws Throwable;
 
 }
