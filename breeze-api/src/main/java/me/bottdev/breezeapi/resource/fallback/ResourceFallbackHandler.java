d7ded6ef1b7349ea268d8d6d260864bc04cd77cb
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-10 23:26:31 +0100

    fallback handler (not implemented yet)

--- breeze-api/src/main/java/me/bottdev/breezeapi/resource/fallback/ResourceFallbackHandler.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/resource/fallback/ResourceFallbackHandler.java
@@ -1,10 +1,9 @@
 package me.bottdev.breezeapi.resource.fallback;
 
 import java.lang.reflect.Method;
-import java.util.Optional;
 
 public interface ResourceFallbackHandler {
 
-    Optional<Object> handle(Method method);
+    Object fallback(Method method);
 
 }
