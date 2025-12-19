735c3e18ed781e36657c74cdddec569ed03f7f8e
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-19 15:39:20 +0100

    hod reload annotation

--- breeze-api/src/main/java/me/bottdev/breezeapi/resource/annotations/HotReload.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/resource/annotations/HotReload.java
@@ -7,6 +7,4 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface Drive {
-    String path() default "";
-}
+public @interface HotReload {}
