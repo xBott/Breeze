4c1d7dc15c0b0283b625efa283d88fd69e9fd919
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-20 23:00:18 +0100

    method for invoking default

--- breeze-api/src/main/java/me/bottdev/breezeapi/di/proxy/ProxyHandler.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/di/proxy/ProxyHandler.java
@@ -1,11 +1,17 @@
 package me.bottdev.breezeapi.di.proxy;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public interface ProxyHandler {
 
     boolean supports(Class<?> iface);
 
-    Object invoke(Class<?> targetClass, Object proxy, Method method, Object[] args) throws Throwable;
+    ProxyResult invoke(Class<?> targetClass, Object proxy, Method method, Object[] args) throws Throwable;
+
+    default ProxyResult invokeDefault(Object proxy, Method method, Object[] args) throws Throwable {
+        Object value = InvocationHandler.invokeDefault(proxy, method, args);
+        return ProxyResult.of(value);
+    }
 
 }
