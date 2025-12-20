ea6e644ea660451bdb427433e4526e0591fb106f
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-20 22:59:24 +0100

    remade using priority list

--- breeze-api/src/main/java/me/bottdev/breezeapi/di/ContextBootstrapper.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/di/ContextBootstrapper.java
@@ -1,33 +1,24 @@
 package me.bottdev.breezeapi.di;
 
+import me.bottdev.breezeapi.commons.priority.PriorityList;
 import me.bottdev.breezeapi.index.BreezeIndex;
 import me.bottdev.breezeapi.index.BreezeIndexBucket;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class ContextBootstrapper {
 
-    private final List<ReaderPriorityWrapper> readers = new ArrayList<>();
+    private final PriorityList<ContextIndexReader<?>> readers = new PriorityList<>();
 
     public ContextBootstrapper addReader(ContextIndexReader<?> reader, int priority) {
-        readers.add(new ReaderPriorityWrapper(reader, priority));
+        readers.add(reader, priority);
         return this;
     }
 
     public void bootstrap(BreezeContext context, ClassLoader classLoader, BreezeIndexBucket bucket) {
-
-        List<ContextIndexReader<?>> sortedReaders = readers.stream()
-                .sorted(Comparator.comparing(ReaderPriorityWrapper::getPriority))
-                .map(ReaderPriorityWrapper::getReader)
-                .collect(Collectors.toList());
-
-        sortedReaders.forEach(reader -> {
-            bucket.getIndices().stream()
-                    .filter(index -> reader.getIndexClass().isAssignableFrom(index.getClass()))
-                    .forEach(index -> callReader(reader, context, classLoader, index));
-        });
-
+        readers.stream().forEach(reader ->
+                bucket.getIndices().stream()
+                        .filter(index -> reader.getIndexClass().isAssignableFrom(index.getClass()))
+                        .forEach(index -> callReader(reader, context, classLoader, index))
+        );
     }
 
     private <T extends BreezeIndex> void callReader(
