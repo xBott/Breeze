b72fe7129b0e0a1fdfcce0c39afbc644117ff49d
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-22 03:05:27 +0100

    fixed bugs

--- breeze-api/src/main/java/me/bottdev/breezeapi/resource/source/types/JarResourceSource.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/resource/source/types/JarResourceSource.java
@@ -1,101 +1,93 @@
 package me.bottdev.breezeapi.resource.source.types;
 
-import lombok.RequiredArgsConstructor;
-import me.bottdev.breezeapi.commons.file.FileCommons;
-import me.bottdev.breezeapi.commons.file.input.BreezeFileReader;
+import me.bottdev.breezeapi.commons.file.input.BreezeStreamReader;
 import me.bottdev.breezeapi.commons.file.output.BreezeFileWriter;
 import me.bottdev.breezeapi.commons.file.temp.TempFile;
 import me.bottdev.breezeapi.commons.file.temp.TempFiles;
 import me.bottdev.breezeapi.log.BreezeLogger;
-import me.bottdev.breezeapi.log.types.SimpleTreeLogger;
+import me.bottdev.breezeapi.log.types.SimpleLogger;
 import me.bottdev.breezeapi.resource.ResourceTree;
-import me.bottdev.breezeapi.resource.annotations.DriveSource;
+import me.bottdev.breezeapi.resource.annotations.sources.JarSource;
 import me.bottdev.breezeapi.resource.source.ResourceSource;
 import me.bottdev.breezeapi.resource.source.SourceType;
 import me.bottdev.breezeapi.resource.types.FileResource;
 import me.bottdev.breezeapi.resource.types.file.SingleFileResource;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
-@RequiredArgsConstructor
 public class JarResourceSource implements ResourceSource {
 
-    private final BreezeLogger logger = new SimpleTreeLogger("JarSource");
-
-    private final Path enginePath;
+    private final BreezeLogger logger = new SimpleLogger("JarSource");
 
     @Override
     public ResourceTree<FileResource> provide(Method method) {
 
         ResourceTree<FileResource> resourceTree = new ResourceTree<>();
 
-        if (!method.isAnnotationPresent(DriveSource.class)) return resourceTree;
-        DriveSource annotation = method.getAnnotation(DriveSource.class);
+        if (!method.isAnnotationPresent(JarSource.class)) return resourceTree;
+        JarSource annotation = method.getAnnotation(JarSource.class);
         String pathString = (annotation.path());
-        boolean createIfAbsent = annotation.createIfAbsent();
-        String defaultValue = annotation.defaultValue();
 
-        Path path = enginePath.resolve(pathString);
+        URL url = getClass().getClassLoader().getResource(pathString);
 
-        if (!Files.exists(path)) {
-            if (!createIfAbsent) return resourceTree;
-
-            try {
-                createFile(path, defaultValue);
-            } catch (IOException ex) {
-                logger.error("Could not create file", ex);
-                return resourceTree;
-            }
+        if (url == null) {
+            logger.warn("Could not find resource {} in jar resources.", pathString);
+            return resourceTree;
         }
 
-        if (Files.isRegularFile(path)) {
-            createSingleFileResource(path).ifPresent(resource ->
-                    resourceTree.add("",  resource)
-            );
+        if (!url.getProtocol().equals("file")) {
+            logger.warn("Resource {} is not file.", pathString);
+            return resourceTree;
+        }
+
+        Path path = Paths.get(url.getPath());
+        if (Files.isDirectory(path)) {
+
+            Map<String, FileResource> resources = createTreeFileResources(pathString);
+            resourceTree.addAll(resources);
 
         } else {
 
-            Map<String, FileResource> resources = createTreeFileResources(path);
-            resourceTree.addAll(resources);
+            createSingleFileResource(pathString).ifPresent(resource ->
+                    resourceTree.add("", resource)
+            );
 
         }
 
         return resourceTree;
     }
 
-    private void createFile(Path path, String defaultValue) throws IOException {
-        File created = FileCommons.createFileOrDirectory(path);
-        BreezeFileWriter.INSTANCE.writeString(created, bufferedWriter ->
-                bufferedWriter.write(defaultValue)
-        );
-    }
+    private Optional<FileResource> createSingleFileResource(String path) {
 
-    private Optional<FileResource> createSingleFileResource(Path path) {
-
-        Path relativePath = enginePath.relativize(path);
-
-        Optional<TempFile> targetOptional = TempFiles.create(relativePath);
+        Optional<TempFile> targetOptional = TempFiles.create(Path.of(path));
         if (targetOptional.isEmpty()) return Optional.empty();
 
-        File source = path.toFile();
         TempFile target = targetOptional.get();
-        target.setSourcePath(path);
 
         try {
 
+            InputStream sourceStream = getClass().getClassLoader().getResourceAsStream(path);
+
+            if (sourceStream == null) {
+                throw new FileNotFoundException(path.toString());
+            }
+
             BreezeFileWriter.INSTANCE.writeChunks(target.toFile(), out ->
-                    BreezeFileReader.INSTANCE.readChunks(source, (data, length) ->
+                    BreezeStreamReader.INSTANCE.readChunks(sourceStream, (data, length) ->
                             out.write(data, 0, length)
                     )
             );
@@ -111,36 +103,101 @@ public class JarResourceSource implements ResourceSource {
         return Optional.empty();
     }
 
-    private Map<String, FileResource> createTreeFileResources(Path root) {
+
+    private Map<String, FileResource> createTreeFileResources(String resourcePath) {
 
         HashMap<String, FileResource> resources = new HashMap<>();
 
         try {
 
-            Files.walkFileTree(root, new SimpleFileVisitor<>() {
-                @Override @NotNull
-                public FileVisitResult visitFile(@NotNull Path file, @NotNull BasicFileAttributes attrs) {
+            Enumeration<URL> urls = getClass().getClassLoader().getResources(resourcePath);
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                String protocol = url.getProtocol();
 
-                    createSingleFileResource(file).ifPresent(resource -> {
+                if ("file".equals(protocol)) {
+                    Map<String, FileResource> read = readResourcesFromFileSystem(resourcePath);
+                    resources.putAll(read);
 
-                        Path relativePath = enginePath.relativize(file);
-                        resources.put(relativePath.toString(), resource);
-
-                    });
-
-                    return FileVisitResult.CONTINUE;
+                } else if ("jar".equals(protocol)) {
+                    Map<String, FileResource> read = readResourcesFromJar(resourcePath);
+                    resources.putAll(read);
                 }
-            });
+            }
 
-        } catch (IOException ex) {
-            logger.error("Could not walk through file tree.", ex);
-            resources.values().forEach(resource ->
-                    TempFiles.delete(resource.getTempFile())
-            );
+        } catch (Exception ex) {
+            logger.error("Could not load resources.", ex);
+            resources.values().forEach(resource -> TempFiles.delete(resource.getTempFile()));
+
         }
 
         return resources;
-
     }
+
+    public Map<String, FileResource> readResourcesFromFileSystem(String resourcePath) throws Exception {
+
+        HashMap<String, FileResource> resources = new HashMap<>();
+        Enumeration<URL> urls = getClass().getClassLoader().getResources(resourcePath);
+
+        while (urls.hasMoreElements()) {
+
+            URL url = urls.nextElement();
+            if (!"file".equals(url.getProtocol())) continue;
+
+            Path dir = Path.of(url.toURI());
+            Files.walk(dir)
+                    .filter(Files::isRegularFile)
+                    .forEach(path -> {
+
+                        Path relativePath = dir.relativize(path);
+                        String pathString = resourcePath + File.separator + relativePath;
+
+                        Optional<FileResource> resourceOptional = createSingleFileResource(pathString);
+                        resourceOptional.ifPresent(resource ->
+                                resources.put(pathString, resource)
+                        );
+
+                    });
+        }
+
+        return resources;
+    }
+
+    public Map<String, FileResource> readResourcesFromJar(String resourcePath) throws IOException {
+
+        HashMap<String, FileResource> resources = new HashMap<>();
+        Enumeration<URL> urls = getClass().getClassLoader().getResources(resourcePath);
+
+        while (urls.hasMoreElements()) {
+
+            URL url = urls.nextElement();
+            if (!"jar".equals(url.getProtocol())) continue;
+
+            String jarPath = url.getPath().substring(5, url.getPath().indexOf("!"));
+            jarPath = URLDecoder.decode(jarPath, StandardCharsets.UTF_8);
+
+            try (JarFile jar = new JarFile(jarPath)) {
+
+                Enumeration<JarEntry> entries = jar.entries();
+                while (entries.hasMoreElements()) {
+                    JarEntry entry = entries.nextElement();
+
+                    if (!entry.isDirectory() && entry.getName().startsWith(resourcePath + "/")) {
+
+                        String relativePath = entry.getName().substring(resourcePath.length() + 1);
+
+                        Optional<FileResource> resourceOptional = createSingleFileResource(relativePath);
+                        resourceOptional.ifPresent(resource ->
+                                resources.put(relativePath, resource)
+                        );
+
+                    }
+                }
+            }
+        }
+
+        return resources;
+    }
+
 
 }
