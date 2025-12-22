7baca8006e1fdb479a8935418118dde5e5d6454b
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-22 03:06:20 +0100

    new tests

--- breeze-api/src/test/java/resource/ResourceProxyTest.java
+++ breeze-api/src/test/java/resource/ResourceProxyTest.java
@@ -3,13 +3,16 @@ package resource;
 import me.bottdev.breezeapi.di.annotations.Proxy;
 import me.bottdev.breezeapi.di.proxy.ProxyFactoryRegistry;
 import me.bottdev.breezeapi.log.types.SimpleTreeLogger;
-import me.bottdev.breezeapi.resource.annotations.DummySource;
+import me.bottdev.breezeapi.resource.ResourceTree;
+import me.bottdev.breezeapi.resource.annotations.sources.DummySource;
 import me.bottdev.breezeapi.resource.annotations.ProvideResource;
+import me.bottdev.breezeapi.resource.annotations.sources.JarSource;
 import me.bottdev.breezeapi.resource.proxy.ResourceProvider;
 import me.bottdev.breezeapi.resource.proxy.ResourceProxyHandlerFactory;
 import me.bottdev.breezeapi.resource.source.ResourceSourceRegistry;
 import me.bottdev.breezeapi.resource.source.SourceType;
 import me.bottdev.breezeapi.resource.source.types.DummyResourceSource;
+import me.bottdev.breezeapi.resource.source.types.JarResourceSource;
 import me.bottdev.breezeapi.resource.types.file.SingleFileResource;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,6 +32,19 @@ public class ResourceProxyTest {
         @DummySource(value = "Version is 0.2!")
         Optional<SingleFileResource> getVersion();
 
+        @ProvideResource
+        @JarSource(path = "test_resource.txt")
+        Optional<SingleFileResource> getTestResource();
+
+        @ProvideResource
+        @JarSource(path = "not_existing.txt")
+        @DummySource(value = "Dummy")
+        Optional<SingleFileResource> getChainedResource();
+
+        @ProvideResource(isTree = true)
+        @JarSource(path = "tree")
+        ResourceTree<SingleFileResource> getTree();
+
     }
 
     static final SimpleTreeLogger logger = new SimpleTreeLogger("ResourceProxyTest");
@@ -40,6 +56,7 @@ public class ResourceProxyTest {
     static void createProxyFactory() {
 
         ResourceSourceRegistry resourceSourceRegistry = new ResourceSourceRegistry()
+                .register(SourceType.JAR, new JarResourceSource())
                 .register(SourceType.DUMMY, new DummyResourceSource());
 
         proxyFactory = new ProxyFactoryRegistry()
@@ -98,6 +115,58 @@ public class ResourceProxyTest {
 
             resource.save();
 
+        });
+
+    }
+
+    @Test
+    void shouldReadJarResource() {
+
+        logger.withSection("Test jar read:", "", () -> {
+            Optional<SingleFileResource> versionOptional = provider.getTestResource();
+
+            String content = versionOptional
+                    .flatMap(SingleFileResource::readTrimmed)
+                    .orElse("Resource is empty!");
+
+            logger.info(" Read content: {}", content);
+
+            assertEquals("BreezeEngine is the best!", content);
+        });
+
+    }
+
+    @Test
+    void shouldReadJarAndDummy() {
+
+        logger.withSection("Test jar and dummy read:", "", () -> {
+            Optional<SingleFileResource> versionOptional = provider.getChainedResource();
+
+            String content = versionOptional
+                    .flatMap(SingleFileResource::readTrimmed)
+                    .orElse("Resource is empty!");
+
+            logger.info(" Read content: {}", content);
+
+            assertEquals("Dummy", content);
+        });
+
+    }
+
+    @Test
+    void shouldReadJarTree() {
+
+        logger.withSection("Test jar tree read:", "", () -> {
+            ResourceTree<SingleFileResource> tree = provider.getTree();
+
+            tree.getData().forEach((key, resource) -> {
+                logger.info(key + ": " + resource.readTrimmed().orElse("Resource is empty!"));
+            });
+
+            int size = tree.getSize();
+            logger.info("Read {} resources", size);
+
+            assertEquals(2, size);
         });
 
     }
