f677e9fdc3d2944e2cc40e7dd62f85fc5f229e13
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-22 03:05:09 +0100

    changed source type

--- breeze-api/src/main/java/me/bottdev/breezeapi/resource/annotations/sources/DummySource.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/resource/annotations/sources/DummySource.java
@@ -1,5 +1,6 @@
-package me.bottdev.breezeapi.resource.annotations;
+package me.bottdev.breezeapi.resource.annotations.sources;
 
+import me.bottdev.breezeapi.resource.annotations.ResourceSourceDef;
 import me.bottdev.breezeapi.resource.source.SourceType;
 
 import java.lang.annotation.ElementType;
@@ -7,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@ResourceSourceDef(type = SourceType.DUMMY, defaultPriority = 2)
+@ResourceSourceDef(type = SourceType.DUMMY, defaultPriority = 10)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface DummySource {
