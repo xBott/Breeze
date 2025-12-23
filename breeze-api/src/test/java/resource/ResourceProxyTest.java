1c1b750ba3e104b55f50228c29321944fbab2940
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-23 22:35:05 +0100

    changed cache

--- breeze-api/src/test/java/resource/ResourceProxyTest.java
+++ breeze-api/src/test/java/resource/ResourceProxyTest.java
@@ -1,5 +1,9 @@
 package resource;
 
+import me.bottdev.breezeapi.cache.CacheManager;
+import me.bottdev.breezeapi.cache.proxy.CacheProxyHandlerFactory;
+import me.bottdev.breezeapi.cache.proxy.Cacheable;
+import me.bottdev.breezeapi.cache.proxy.annotations.CachePut;
 import me.bottdev.breezeapi.di.annotations.Proxy;
 import me.bottdev.breezeapi.di.proxy.ProxyFactoryRegistry;
 import me.bottdev.breezeapi.log.types.SimpleTreeLogger;
@@ -26,21 +30,30 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class ResourceProxyTest {
 
     @Proxy
-    public interface SomeResourceProvider extends ResourceProvider {
+    public interface SomeResourceProvider extends ResourceProvider, Cacheable {
 
+        @CachePut
         @ProvideResource
         @DummySource(value = "Version is 0.2!")
         Optional<SingleFileResource> getVersion();
 
+        @CachePut
+        @ProvideResource
+        @DummySource(value = "Message to edit")
+        Optional<SingleFileResource> getMutableResource();
+
+        @CachePut
         @ProvideResource
         @JarSource(path = "test_resource.txt")
         Optional<SingleFileResource> getTestResource();
 
+        @CachePut
         @ProvideResource
         @JarSource(path = "not_existing.txt")
         @DummySource(value = "Dummy")
         Optional<SingleFileResource> getChainedResource();
 
+        @CachePut
         @ProvideResource(isTree = true)
         @JarSource(path = "tree")
         ResourceTree<SingleFileResource> getTree();
@@ -50,17 +63,21 @@ public class ResourceProxyTest {
     static final SimpleTreeLogger logger = new SimpleTreeLogger("ResourceProxyTest");
 
     static ProxyFactoryRegistry proxyFactory;
+    static CacheManager cacheManager;
     SomeResourceProvider provider;
 
     @BeforeAll
     static void createProxyFactory() {
+
+        cacheManager = new CacheManager();
 
         ResourceSourceRegistry resourceSourceRegistry = new ResourceSourceRegistry()
                 .register(SourceType.JAR, new JarResourceSource())
                 .register(SourceType.DUMMY, new DummyResourceSource());
 
         proxyFactory = new ProxyFactoryRegistry()
-                .register(new ResourceProxyHandlerFactory(resourceSourceRegistry), 0);
+                .register(new CacheProxyHandlerFactory(cacheManager), 0)
+                .register(new ResourceProxyHandlerFactory(resourceSourceRegistry), 1);
     }
 
     @BeforeEach
@@ -97,7 +114,7 @@ public class ResourceProxyTest {
 
         logger.withSection("Test read and write:", "", () -> {
 
-            Optional<SingleFileResource> versionOptional = provider.getVersion();
+            Optional<SingleFileResource> versionOptional = provider.getMutableResource();
             if (versionOptional.isEmpty()) return;
 
             SingleFileResource resource = versionOptional.get();
@@ -105,9 +122,10 @@ public class ResourceProxyTest {
             String contentBefore = resource.readTrimmed().orElse("Resource is empty!");
             logger.info(" (Before) Read content: {}", contentBefore);
 
-            assertEquals("Version is 0.2!", contentBefore);
+            assertEquals("Message to edit", contentBefore);
 
             resource.write("Updated data");
+
             String contentAfter = resource.readTrimmed().orElse("Resource is empty!");
             logger.info(" (After) Read content: {}", contentAfter);
 
