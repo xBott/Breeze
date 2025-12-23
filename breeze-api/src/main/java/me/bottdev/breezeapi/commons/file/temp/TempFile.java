4af7de98ae9dfcfc070202ceacf64a1df59187a9
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-23 22:32:00 +0100

    file update method. Updates data from source to temp

--- breeze-api/src/main/java/me/bottdev/breezeapi/commons/file/temp/TempFile.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/commons/file/temp/TempFile.java
@@ -3,8 +3,11 @@ package me.bottdev.breezeapi.commons.file.temp;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import me.bottdev.breezeapi.commons.file.input.BreezeFileReader;
+import me.bottdev.breezeapi.commons.file.output.BreezeFileWriter;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
@@ -44,6 +47,23 @@ public class TempFile {
     public Optional<File> getSourceFile() {
         if (sourcePath == null) return Optional.empty();
         return Optional.of(sourcePath.toFile());
+    }
+
+    public void update() throws IOException {
+
+        Optional<File> sourceFileOptional = getSourceFile();
+        if (sourceFileOptional.isEmpty()) return;
+
+        File sourceFile = sourceFileOptional.get();
+
+        BreezeFileWriter.INSTANCE.writeChunks(toFile(), out -> {
+
+            BreezeFileReader.INSTANCE.readChunks(sourceFile, (data, length) -> {
+                out.write(data, 0, length);
+            });
+
+        });
+
     }
 
 }
