5868a82741b51092c97bade35376f04fc50e76f4
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-24 04:10:25 +0100

    resource proxy handler that supports resource watcher

---
+++ breeze-api/src/main/java/me/bottdev/breezeapi/resource/proxy/types/HotReloadResourceProxyHandler.java
@@ -0,0 +1,60 @@
+package me.bottdev.breezeapi.resource.proxy.types;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import me.bottdev.breezeapi.cache.Cache;
+import me.bottdev.breezeapi.cache.CacheManager;
+import me.bottdev.breezeapi.resource.annotations.HotReload;
+import me.bottdev.breezeapi.resource.proxy.ResourceProxyHandler;
+import me.bottdev.breezeapi.resource.source.ResourceSourceRegistry;
+import me.bottdev.breezeapi.resource.types.FileResource;
+import me.bottdev.breezeapi.resource.watcher.ResourceWatcher;
+
+import java.lang.reflect.Method;
+
+@RequiredArgsConstructor
+public class HotReloadResourceProxyHandler implements ResourceProxyHandler {
+
+    @Getter
+    private final ResourceSourceRegistry resourceSourceRegistry;
+    private final ResourceWatcher resourceWatcher;
+    private final CacheManager cacheManager;
+
+    @Override
+    public Object provideResult(Method method) {
+        Object result = ResourceProxyHandler.super.provideResult(method);
+        registerResultInWatcher(method, result);
+        return result;
+
+    }
+
+    private void registerResultInWatcher(Method method, Object result) {
+        if (result == null || !method.isAnnotationPresent(HotReload.class)) return;
+
+        if (result instanceof FileResource resource) {
+
+            resourceWatcher.registerResource(resource);
+            if (!resourceWatcher.isRegistered(resource)) return;
+
+            resourceWatcher.getHookContainer(resource).add(changedResource ->
+                    onChange(method, changedResource)
+            );
+
+        }
+    }
+
+    private void onChange(Method method, FileResource changedResource) {
+        HotReload hotReload = method.getAnnotation(HotReload.class);
+        boolean evictCache = hotReload.evictCache();
+        if (evictCache) {
+            String cacheGroup = hotReload.cacheGroup();
+            evictCache(cacheGroup);
+        }
+        changedResource.readTrimmed().ifPresent(System.out::println);
+    }
+
+    private void evictCache(String cacheGroup) {
+        cacheManager.get(cacheGroup).ifPresent(Cache::clear);
+    }
+
+}
