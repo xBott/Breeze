e3b8b789d051ad86dfe91f7c1f57e913399f55bc
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-31 03:14:19 +0100

    added default fallback field and some methods for creating simple i18n container

--- breeze-api/src/main/java/me/bottdev/breezeapi/i18n/TranslationModuleManager.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/i18n/TranslationModuleManager.java
@@ -1,10 +1,15 @@
 package me.bottdev.breezeapi.i18n;
 
+import lombok.Getter;
+import me.bottdev.breezeapi.i18n.types.SimpleI18n;
+
 import java.util.*;
 
 public class TranslationModuleManager {
 
     private final Map<String, TranslationModule> modules = new HashMap<>();
+    @Getter
+    private final Locale defaultFallback = Locale.forLanguageTag("en-US");
 
     public List<TranslationModule> getModules() {
         return new ArrayList<>(modules.values());
@@ -29,6 +34,14 @@ public class TranslationModuleManager {
         }
 
         return Optional.empty();
+    }
+
+    public I18n simple(Locale locale, Locale fallback) {
+        return new SimpleI18n(locale, fallback, this);
+    }
+
+    public I18n simple(Locale locale) {
+        return simple(locale, defaultFallback);
     }
 
 }
