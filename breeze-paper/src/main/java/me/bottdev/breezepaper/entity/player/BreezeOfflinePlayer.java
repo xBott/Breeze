4a217487c90e8104e34d16c987fe15a19e783623
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-31 03:11:55 +0100

    removed method for getting representation of online player

--- breeze-paper/src/main/java/me/bottdev/breezepaper/entity/player/BreezeOfflinePlayer.java
+++ breeze-paper/src/main/java/me/bottdev/breezepaper/entity/player/BreezeOfflinePlayer.java
@@ -2,9 +2,7 @@ package me.bottdev.breezepaper.entity.player;
 
 import lombok.Getter;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.entity.Player;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public class BreezeOfflinePlayer implements BreezePlayer {
@@ -24,12 +22,6 @@ public class BreezeOfflinePlayer implements BreezePlayer {
     @Override
     public String getName() {
         return offlinePlayer.getName();
-    }
-
-    public Optional<BreezeOnlinePlayer> getOnlinePlayer() {
-        Player onlinePlayer = offlinePlayer.getPlayer();
-        if (onlinePlayer == null) return Optional.empty();
-        return Optional.of(new BreezeOnlinePlayer(onlinePlayer));
     }
 
 }
