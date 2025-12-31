84c6be9bc46033a9803644a91f895298ee2db9bb
_xBott <roma.plakhotniuk07@gmail.com>
2025-12-31 03:11:34 +0100

    added support of i18n system

---
+++ breeze-paper/src/main/java/me/bottdev/breezepaper/chat/TranslatableMessageReceiver.java
@@ -0,0 +1,19 @@
+package me.bottdev.breezepaper.chat;
+
+import me.bottdev.breezeapi.i18n.I18n;
+
+public interface TranslatableMessageReceiver extends MessageReceiver {
+
+    I18n getI18n();
+
+    default void sendTranslatedMessage(String key, Object... args) {
+        String message = getI18n().get(key, args);
+        sendMessage(message);
+    }
+
+    default void sendTranslatedMessage(String key) {
+        String message = getI18n().get(key);
+        sendMessage(message);
+    }
+
+}
