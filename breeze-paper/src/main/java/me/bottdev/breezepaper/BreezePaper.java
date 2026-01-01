67e8dc6a216ccb829895395bc616855e3c6d7b00
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-02 00:25:17 +0100

    now player manager is a simple field, added registration of command node factories and so

--- breeze-paper/src/main/java/me/bottdev/breezepaper/BreezePaper.java
+++ breeze-paper/src/main/java/me/bottdev/breezepaper/BreezePaper.java
@@ -3,12 +3,20 @@ package me.bottdev.breezepaper;
 import lombok.Getter;
 import me.bottdev.breezeapi.BreezeEngine;
 import me.bottdev.breezeapi.command.Command;
+import me.bottdev.breezeapi.command.CommandTreeParser;
+import me.bottdev.breezeapi.command.argument.CommandArgumentFactory;
+import me.bottdev.breezeapi.command.nodes.CommandExecuteNode;
+import me.bottdev.breezeapi.command.nodes.CommandLiteralNode;
 import me.bottdev.breezeapi.di.annotations.Inject;
 import me.bottdev.breezeapi.modules.ModuleManager;
 import me.bottdev.breezecore.modules.loaders.DependencyModuleLoader;
 import me.bottdev.breezecore.SimpleBreezeEngine;
-import me.bottdev.breezepaper.autoloaders.PaperCommandAutoloader;
-import me.bottdev.breezepaper.entity.player.BreezePlayerManager;
+import me.bottdev.breezepaper.autoloaders.PaperCommandAutoLoader;
+import me.bottdev.breezepaper.command.PaperCommandContextFactory;
+import me.bottdev.breezepaper.command.PaperCommandRegistrar;
+import me.bottdev.breezepaper.command.nodes.PaperExecuteNodeFactory;
+import me.bottdev.breezepaper.command.nodes.PaperLiteralNodeFactory;
+import me.bottdev.breezepaper.entity.player.PlayerManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.nio.file.Path;
@@ -19,30 +27,25 @@ public class BreezePaper extends JavaPlugin {
     private static BreezePaper instance;
     @Getter
     private BreezeEngine engine;
-
-    @Inject
-    private BreezePlayerManager playerManager;
+    @Getter
+    private PlayerManager playerManager;
 
     @Override
     public void onEnable() {
         instance = this;
         engine = new SimpleBreezeEngine(getDataPath().toAbsolutePath());
+        playerManager = new PlayerManager(engine.getTranslationModuleManager());
 
         addDependencyModuleLoader();
         addCommandAutoloader();
 
         engine.start();
-        //engine.getContext().createComponent("breezeCommand", SupplyType.SINGLETON, new BreezeCommand());
+        createBreezeCommand();
 
         playerManager.getPlayers().forEach(player ->
                 player.sendMessage("Location of player {} is {}", player.getName(), player.getLocation())
         );
 
-    }
-
-    @Override
-    public void onDisable() {
-        engine.stop();
     }
 
     private void addDependencyModuleLoader() {
@@ -54,7 +57,28 @@ public class BreezePaper extends JavaPlugin {
     }
 
     private void addCommandAutoloader() {
-        engine.getAutoLoaderRegistry().register(Command.class, new PaperCommandAutoloader(this));
+
+        PaperCommandContextFactory contextFactory = new PaperCommandContextFactory(playerManager);
+
+        PaperCommandRegistrar registrar = new PaperCommandRegistrar(this);
+        registrar.addFactory(CommandLiteralNode.class, new PaperLiteralNodeFactory());
+        registrar.addFactory(CommandExecuteNode.class, new PaperExecuteNodeFactory(contextFactory));
+
+        CommandArgumentFactory argumentFactory = CommandArgumentFactory.defaultFactory();
+        CommandTreeParser parser = new CommandTreeParser(argumentFactory);
+
+        engine.getAutoLoaderRegistry().register(Command.class, new PaperCommandAutoLoader(parser, registrar));
+
+    }
+
+    private void createBreezeCommand() {
+        //engine.getContext().createComponent("breezeCommand", SupplyType.SINGLETON, BreezeCommand.class);
+
+    }
+
+    @Override
+    public void onDisable() {
+        engine.stop();
     }
 
 }
