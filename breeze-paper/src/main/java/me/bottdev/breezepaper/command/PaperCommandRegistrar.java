ec2dd830d0431f7332d5fc98d808a80151233a9e
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-02 00:24:26 +0100

    paper command registration system based on breeze command system

--- breeze-paper/src/main/java/me/bottdev/breezepaper/command/PaperCommandRegistrar.java
+++ breeze-paper/src/main/java/me/bottdev/breezepaper/command/PaperCommandRegistrar.java
@@ -1,24 +1,53 @@
 package me.bottdev.breezepaper.command;
 
+import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import lombok.RequiredArgsConstructor;
 import me.bottdev.breezeapi.command.CommandNode;
 import me.bottdev.breezeapi.command.nodes.CommandRootNode;
+import me.bottdev.breezeapi.log.BreezeLogger;
+import me.bottdev.breezeapi.log.types.SimpleLogger;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RequiredArgsConstructor
 public class PaperCommandRegistrar {
 
-    public static void register(CommandRootNode rootNode, JavaPlugin plugin) {
-        LiteralCommandNode<CommandSourceStack> paperRoot = convert(rootNode).build();
+    private final BreezeLogger logger = new SimpleLogger("PaperCommandRegistrar");
+    private final Map<Class<? extends CommandNode>, PaperCommandNodeFactory> factories = new HashMap<>();
+
+    private final JavaPlugin plugin;
+
+
+    public PaperCommandRegistrar addFactory(
+            Class<? extends CommandNode> nodeClass,
+            PaperCommandNodeFactory builder
+    ) {
+        factories.put(nodeClass, builder);
+        return this;
+    }
+
+    private Optional<PaperCommandNodeFactory> getFactory(
+            Class<? extends CommandNode> nodeClass
+    ) {
+        return Optional.ofNullable(factories.get(nodeClass));
+    }
+
+    public void register(CommandRootNode rootNode) {
         plugin.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
+            LiteralCommandNode<CommandSourceStack> paperRoot = convert(rootNode).build();
             commands.registrar().register(paperRoot);
         });
     }
 
-    private static LiteralArgumentBuilder<CommandSourceStack> convert(CommandRootNode rootNode) {
+    private LiteralArgumentBuilder<CommandSourceStack> convert(CommandRootNode rootNode) {
         LiteralArgumentBuilder<CommandSourceStack> paperRoot = Commands.literal(rootNode.getName());
 
         rootNode.getChildren().values().forEach(child ->
@@ -28,17 +57,29 @@ public class PaperCommandRegistrar {
         return paperRoot;
     }
 
-    private static void registerSubCommand(CommandNode commandNode, LiteralArgumentBuilder<CommandSourceStack> paperParent) {
+    private void registerSubCommand(
+            CommandNode node,
+            ArgumentBuilder<CommandSourceStack, ?> parent
+    ) {
 
-        if (!commandNode.hasChildren()) return;
+        Class<? extends CommandNode> nodeClass = node.getClass();
+        logger.info("registering node of class {} and name {}", nodeClass, node.getDisplayName());
 
-        LiteralArgumentBuilder<CommandSourceStack> paperNode = Commands.literal(commandNode.getValue());
-        paperParent.then(paperNode);
+        Optional<PaperCommandNodeFactory> factoryOptional = getFactory(nodeClass);
+        if (factoryOptional.isEmpty()) {
+            logger.warn("No command factory found for class {}", nodeClass);
+            return;
+        }
 
-        commandNode.getChildren().values().forEach(child ->
-                registerSubCommand(child, paperParent)
+        PaperCommandNodeFactory factory = factoryOptional.get();
+        ArgumentBuilder<CommandSourceStack, ?> paperNode = factory.create(parent, node);
+
+        if (!node.hasChildren()) return;
+        node.getChildren().values().forEach(child ->
+                registerSubCommand(child, paperNode)
         );
 
+        parent.then(paperNode);
     }
 
 }
