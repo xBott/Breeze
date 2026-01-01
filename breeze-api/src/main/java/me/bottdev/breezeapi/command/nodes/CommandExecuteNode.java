ab25828a33020f7b12224a99a11d8a8894132953
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-02 00:22:49 +0100

    generation of command scheme and execution logic

--- breeze-api/src/main/java/me/bottdev/breezeapi/command/nodes/CommandExecuteNode.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/command/nodes/CommandExecuteNode.java
@@ -1,18 +1,32 @@
 package me.bottdev.breezeapi.command.nodes;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import me.bottdev.breezeapi.command.CommandExecutionContext;
 import me.bottdev.breezeapi.command.CommandNode;
+import me.bottdev.breezeapi.command.exceptions.SubCommandResolveException;
+import me.bottdev.breezeapi.command.exceptions.UnsupportedParameterException;
+import me.bottdev.breezeapi.command.scheme.CommandScheme;
+import me.bottdev.breezeapi.log.BreezeLogger;
+import me.bottdev.breezeapi.log.types.SimpleLogger;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
 
 @Getter
-@RequiredArgsConstructor
 public class CommandExecuteNode implements CommandNode {
 
+    private final BreezeLogger logger = new SimpleLogger("CommandExecuteNode");
+
     private final Object commandInstance;
-    private final Method handler;
+    private final Method method;
+    private final CommandScheme scheme;
+
+    public CommandExecuteNode(Object commandInstance, Method handler) throws UnsupportedParameterException {
+        this.commandInstance = commandInstance;
+        this.method = handler;
+        this.scheme = CommandScheme.ofMethod(handler);
+    }
 
     @Override
     public String getValue() {
@@ -32,6 +46,17 @@ public class CommandExecuteNode implements CommandNode {
     @Override
     public Optional<CommandNode> getChild(String value) {
         return Optional.empty();
+    }
+
+    public void execute(CommandExecutionContext context) {
+        try {
+            Object[] args = scheme.resolve(context);
+            method.invoke(commandInstance, args);
+
+        } catch (SubCommandResolveException | IllegalAccessException | InvocationTargetException ex) {
+            logger.error("Failed to execute command", ex);
+
+        }
     }
 
 }
