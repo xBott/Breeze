e01c9bbc0fe8bf9879bbb1be721d3c527553088e
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-06 17:13:17 +0100

    changed constructor

--- breeze-admin/src/main/java/me/bottdev/breezeadmin/AdminModule.java
+++ breeze-admin/src/main/java/me/bottdev/breezeadmin/AdminModule.java
@@ -1,7 +1,6 @@
 package me.bottdev.breezeadmin;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import me.bottdev.breezeadmin.config.AdminConfigLoader;
 import me.bottdev.breezeadmin.translation.AdminTranslationLoader;
 import me.bottdev.breezeapi.di.annotations.Inject;
@@ -9,25 +8,29 @@ import me.bottdev.breezeapi.i18n.TranslationModule;
 import me.bottdev.breezeapi.log.BreezeLogger;
 import me.bottdev.breezeapi.log.types.SimpleLogger;
 import me.bottdev.breezeapi.modules.Module;
+import me.bottdev.breezeapi.modules.ModuleDescriptor;
 import me.bottdev.breezeapi.modules.ModuleStatus;
 import me.bottdev.breezeapi.modules.annotations.ModuleInfo;
 
 import java.io.File;
 
+
 @ModuleInfo(name = "Admin", version = "0.0.1")
-@RequiredArgsConstructor
 public class AdminModule extends Module {
 
     @Getter
     private final BreezeLogger logger = new SimpleLogger("BreezeAdmin");
 
-    private final File dataFolder;
     private ModuleStatus moduleStatus = ModuleStatus.DISABLED;
 
     @Inject
     private AdminConfigLoader adminConfigLoader;
     @Inject
     private AdminTranslationLoader adminTranslationLoader;
+
+    public AdminModule(File dataFolder, ModuleDescriptor descriptor) {
+        super(dataFolder, descriptor);
+    }
 
     @Override
     public ModuleStatus getStatus() {
