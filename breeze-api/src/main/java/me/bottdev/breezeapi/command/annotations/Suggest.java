eabf1f960ca9a8d70257b249cea3e6d582732680
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-06 17:16:37 +0100

    added default factory

--- breeze-api/src/main/java/me/bottdev/breezeapi/command/annotations/Suggest.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/command/annotations/Suggest.java
@@ -1,6 +1,7 @@
 package me.bottdev.breezeapi.command.annotations;
 
 import me.bottdev.breezeapi.command.suggestion.SuggestionProviderFactory;
+import me.bottdev.breezeapi.command.suggestion.types.EmptySuggestionFactory;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -8,7 +9,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target(ElementType.PARAMETER)
 public @interface Suggest {
-    Class<? extends SuggestionProviderFactory> value();
+    Class<? extends SuggestionProviderFactory> value() default EmptySuggestionFactory.class;
+
 }
