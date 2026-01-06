9fb6aeb9b4be63d18a3d650152f0d73445393adb
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-06 17:15:43 +0100

    added ranges

--- breeze-api/src/main/java/me/bottdev/breezeapi/command/argument/types/FloatArgument.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/command/argument/types/FloatArgument.java
@@ -1,14 +1,18 @@
 package me.bottdev.breezeapi.command.argument.types;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import me.bottdev.breezeapi.command.argument.CommandArgument;
 
+@Getter
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class FloatArgument implements CommandArgument<Float> {
 
-    @Getter
     private final String name;
+    private float min = Integer.MIN_VALUE;
+    private float max = Integer.MAX_VALUE;
 
     @Override
     public Class<Float> getType() {
--- breeze-api/src/main/java/me/bottdev/breezeapi/command/argument/types/IntegerArgument.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/command/argument/types/IntegerArgument.java
@@ -1,14 +1,18 @@
 package me.bottdev.breezeapi.command.argument.types;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import me.bottdev.breezeapi.command.argument.CommandArgument;
 
+@Getter
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class IntegerArgument implements CommandArgument<Integer> {
 
-    @Getter
     private final String name;
+    private int min = Integer.MIN_VALUE;
+    private int max = Integer.MAX_VALUE;
 
     @Override
     public Class<Integer> getType() {
