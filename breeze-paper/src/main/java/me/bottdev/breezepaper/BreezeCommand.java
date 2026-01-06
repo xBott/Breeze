e95c8d7b3b9f894c4676f6a45d51b847745086d4
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-06 17:13:36 +0100

    added some roots

--- breeze-paper/src/main/java/me/bottdev/breezepaper/BreezeCommand.java
+++ breeze-paper/src/main/java/me/bottdev/breezepaper/BreezeCommand.java
@@ -1,13 +1,31 @@
 package me.bottdev.breezepaper;
 
+import me.bottdev.breezeapi.BreezeEngine;
 import me.bottdev.breezeapi.command.Command;
 import me.bottdev.breezeapi.command.CommandSender;
+import me.bottdev.breezeapi.command.annotations.Argument;
 import me.bottdev.breezeapi.command.annotations.Sender;
 import me.bottdev.breezeapi.command.annotations.SubCommand;
+import me.bottdev.breezeapi.command.annotations.Suggest;
 import me.bottdev.breezeapi.di.annotations.Component;
+import me.bottdev.breezeapi.di.annotations.Inject;
+import me.bottdev.breezeapi.modules.ModuleDescriptor;
+import me.bottdev.breezeapi.modules.ModuleManager;
+import me.bottdev.breezeapi.modules.ModuleStatus;
+
+import java.io.File;
 
 @Component
 public class BreezeCommand implements Command {
+
+    private final BreezeEngine breezeEngine;
+    private final ModuleManager moduleManager;
+
+    @Inject
+    public BreezeCommand(BreezeEngine breezeEngine, ModuleManager moduleManager) {
+        this.breezeEngine = breezeEngine;
+        this.moduleManager = moduleManager;
+    }
 
     @Override
     public String getName() {
@@ -28,25 +46,58 @@ public class BreezeCommand implements Command {
         sender.send("Version of BreezePaper is ....");
     }
 
+    @SubCommand(path = "restart")
+    public void modulesReload(
+            @Sender CommandSender sender
+    ) {
+        breezeEngine.restart();
+    }
+
     @SubCommand(path = "modules")
     public void modules(
             @Sender CommandSender sender
     ) {
-        sender.send("modules list / reload");
+        sender.send("use modules list");
     }
 
     @SubCommand(path = "modules list")
     public void modulesList(
             @Sender CommandSender sender
     ) {
-        sender.send("Here's a list of modules....");
+        sender.send("List of modules:");
+        moduleManager.getModules().forEach(module -> {
+            ModuleDescriptor descriptor = module.getDescriptor();
+            String name = descriptor.getName();
+            String version = descriptor.getVersion();
+            ModuleStatus status = module.getStatus();
+            sender.send(" <gray>- <white>{} <yellow>{} <light_purple>{}", name, version, status);
+        });
     }
 
-    @SubCommand(path = "modules reload")
-    public void modulesReload(
-            @Sender CommandSender sender
+    @SubCommand(path = "modules info <name>")
+    public void moduleInfo(
+            @Sender CommandSender sender,
+            @Argument(name = "name") @Suggest() String moduleName
     ) {
-        sender.send("Modules are reloaded!");
+
+        moduleManager.getModules().stream()
+                .filter(module -> module.getDescriptor().getName().equals(moduleName))
+                .findFirst()
+                .ifPresent(module -> {
+
+                    ModuleDescriptor descriptor = module.getDescriptor();
+                    String name = descriptor.getName();
+                    String version = descriptor.getVersion();
+                    ModuleStatus status = module.getStatus();
+                    File dataFolder = module.getDataFolder();
+
+                    sender.send("Information about module <light_purple>{}<white>:", name);
+                    sender.send("  <gray>Status: <yellow>{}", status);
+                    sender.send("  <gray>Version: <yellow>{}", version);
+                    sender.send("  <gray>Data Folder: <yellow>{}>",  dataFolder.getAbsolutePath());
+
+                });
+
     }
 
 }
