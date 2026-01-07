8f7e200d75545c1f8c622b1aecebc017497179cf
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-07 23:48:03 +0100

    updated build settings

---
+++ breeze-velocity/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "me.bottdev"
+version = "0.2"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
+    annotationProcessor("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
