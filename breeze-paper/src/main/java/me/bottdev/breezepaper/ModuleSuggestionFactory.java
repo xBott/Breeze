c9c60439630ff7db61ee74f4e78860b4d0634f6b
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-08 03:22:47 +0100

    added suggestion factory for modules

---
+++ breeze-paper/src/main/java/me/bottdev/breezepaper/ModuleSuggestionFactory.java
@@ -0,0 +1,25 @@
+package me.bottdev.breezepaper;
+
+import me.bottdev.breezeapi.command.argument.suggestion.SuggestionFactory;
+import me.bottdev.breezeapi.command.argument.suggestion.SuggestionProvider;
+import me.bottdev.breezeapi.di.annotations.Inject;
+import me.bottdev.breezeapi.modules.ModuleManager;
+
+import java.util.stream.Collectors;
+
+public class ModuleSuggestionFactory implements SuggestionFactory {
+
+    private final ModuleManager moduleManager;
+
+    @Inject
+    public ModuleSuggestionFactory(ModuleManager moduleManager) {
+        this.moduleManager = moduleManager;
+    }
+
+    @Override
+    public SuggestionProvider create() {
+        return () -> moduleManager.getModules().stream()
+                .map(module -> module.getDescriptor().getName())
+                .collect(Collectors.toList());
+    }
+}
