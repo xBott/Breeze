9ea51953abf5254720e14f5544a810aa1045d31c
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-08 23:42:21 +0100

    added breeze-mc dependency

--- breeze-admin/build.gradle.kts
+++ breeze-admin/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(project(":breeze-api"))
+    implementation(project(":breeze-mc"))
     implementation(project(":breeze-paper"))
     implementation(project(":breeze-core"))
     annotationProcessor(project(":breeze-processor"))
