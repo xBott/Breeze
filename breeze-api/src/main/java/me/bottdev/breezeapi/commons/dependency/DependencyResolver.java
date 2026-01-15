1d82c03c6640d70e2bc82c0407ee2fd5b3fe8b4d
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-15 16:26:40 +0100

    removed logger getter

--- breeze-api/src/main/java/me/bottdev/breezeapi/commons/dependency/DependencyResolver.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/commons/dependency/DependencyResolver.java
@@ -1,12 +1,8 @@
-package me.bottdev.breezeapi.di.dependency;
-
-import me.bottdev.breezeapi.log.BreezeLogger;
+package me.bottdev.breezeapi.commons.dependency;
 
 import java.util.List;
 
 public interface DependencyResolver<D extends Dependent, C extends DependentContainer<D>> {
-
-    BreezeLogger getLogger();
 
     List<D> resolve(C container);
 
