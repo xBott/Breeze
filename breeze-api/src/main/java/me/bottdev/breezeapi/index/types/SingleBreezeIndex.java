f981ba56043a75b065c05068ce93ae18f9e9bea2
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-15 16:29:24 +0100

    deleted

--- breeze-api/src/main/java/me/bottdev/breezeapi/index/types/SingleBreezeIndex.java
+++
@@ -1,9 +0,0 @@
-package me.bottdev.breezeapi.index.types;
-
-import me.bottdev.breezeapi.index.BreezeIndex;
-
-public interface SingleBreezeIndex extends BreezeIndex {
-
-    String getClassPath();
-
-}
