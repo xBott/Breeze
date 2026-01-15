9ff1c95cf70e88a9be2c1c8c13acb07ca68c72ad
_xBott <roma.plakhotniuk07@gmail.com>
2026-01-15 16:44:47 +0100

    renamed method readIndex to jsut "read"

--- breeze-api/src/main/java/me/bottdev/breezeapi/di/ContextIndexReader.java
+++ breeze-api/src/main/java/me/bottdev/breezeapi/di/ContextIndexReader.java
@@ -1,11 +1,12 @@
 package me.bottdev.breezeapi.di;
 
+import me.bottdev.breezeapi.di.exceptions.ContextReadException;
 import me.bottdev.breezeapi.index.BreezeIndex;
 
 public interface ContextIndexReader<T extends BreezeIndex> {
 
     Class<T> getIndexClass();
 
-    void readIndex(BreezeContext context, ClassLoader classLoader, T index);
+    void read(BreezeContext context, ClassLoader classLoader, T index) throws ContextReadException;
 
 }
